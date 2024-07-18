92468cc5c1d28cdcb9e626554d3205264d4627fc
Simon HARVEY <simon.harvey.86@gmail.com>
2024-07-18 14:54:17 -0400

    Basix nix config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,153 +1,54 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-{ config, lib, pkgs, meta, ... }:
-
-{
-  imports =
-    [ # Include the results of the hardware scan.
-    ];
-
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = meta.hostname; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "us";
-    #useXkbConfig = true; # use xkb.options in tty.
-  };
-
-  # Fixes for longhorn
-  # systemd.tmpfiles.rules = [
-  #   "L+ /usr/local/bin - - - - /run/current-system/sw/bin/"
-  # ];
-  # virtualisation.docker.logDriver = "json-file";
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-  # services.k3s = {
-  #   enable = true;
-  #   role = "server";
-  #   tokenFile = /var/lib/rancher/k3s/server/token;
-  #   extraFlags = toString ([
-	#     "--write-kubeconfig-mode \"0644\""
-	#     "--cluster-init"
-	#     "--disable servicelb"
-	#     "--disable traefik"
-	#     "--disable local-storage"
-  #   ] ++ (if meta.hostname == "homelab-0" then [] else [
-	#       "--server https://homelab-0:6443"
-  #   ]));
-  #   clusterInit = (meta.hostname == "homelab-0");
-  # };
-
-  # services.openiscsi = {
-  #   enable = true;
-  #   name = "iqn.2016-04.com.open-iscsi:${meta.hostname}";
-  # };
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.nixos = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-    packages = with pkgs; [
-      tree
-    ];
-    # Created using mkpasswd -m sha-512
-    hashedPassword = "$6$jWJVLdua9W2REbSy$kZoEBsj8eYvLzyl3Khqxt16tKQkQu83nRHWZy32L9NlFurFuzESjaFjVbcJJizcqCRr3rToHAK8WLgoNBjvPD.";
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIANVi1BTFY9ymZ8jpY3TfsqV6Lo4KtMyFk2h8CDysMF1 nixos@nixos"
-    ];
-  };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-     cifs-utils
-     git
-     k3s
-     neovim
-     nfs-utils
+{ modulesPath, config, lib, pkgs, ... }: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
 
-  # List services that you want to enable:
+  boot.loader.grub = {
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  # services.openssh.passwordAuthentication = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ 80 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  networking.firewall.enable = false;
+  networking = {
+    hostName = "nixos"; # Define your hostname.
+    firewall.enable = true;
+  };
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  time.timeZone = "America/Toronto";
+  i18n.defaultLocale = "en_US.UTF-8";
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "23.11"; # Did you read the comment?
+  programs.fish.enable = true;
 
+  security.sudo.wheelNeedsPassword = false;
+
+  services = {
+    openssh = {
+      enable = true;
+      settings.PasswordAuthentication = false;
+    };
+  };
+
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.curl
+    pkgs.gitMinimal
+  ];
+
+  users.users = {
+
+    root = {
+      openssh.authorizedKeys.keys = [
+        "CHANGE-PUBKEY1"
+        "CHANGE-PUBKEY2"
+      ];
+    };
+  };
+
+  system.stateVersion = "23.11";
 }
--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,33 +1,16 @@
 {
-  description = "Homelab NixOS Flake";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.disko.url = "github:nix-community/disko";
+  inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    # Disko
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = { self, nixpkgs, disko, ... }@inputs: let
-    nodes = [
-      "homelab-0"
-    ];
-  in {
-    nixosConfigurations = builtins.listToAttrs (map (name: {
-	    name = name;
-	    value = nixpkgs.lib.nixosSystem {
-     	    specialArgs = {
-            meta = { hostname = name; };
-          };
-          system = "x86_64-linux";
-          modules = [
-              # Modules
-	            disko.nixosModules.disko
-	            ./hardware-configuration.nix
-	            ./disko-config.nix
-	            ./configuration.nix
-	          ];
-        };
-    }) nodes);
-  };
+  outputs = { nixpkgs, disko, ... }:
+    {
+      nixosConfigurations.nixos-anywhere-vm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          disko.nixosModules.disko
+          ./configuration.nix
+        ];
+      };
+    };
 }
